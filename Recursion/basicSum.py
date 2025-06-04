cf4c361c88ad8d27d67361b61874cde28820854e
Alexander Commodore <CommodoreAlex@users.noreply.github.com>
2025-06-03 20:40:46 -0700

    Create basicSum.py

---
+++ Recursion/basicSum.py
@@ -0,0 +1,13 @@
+# QuickSort | Divide and Conquer (D&C) Algorithm (recursive)
+# Figure out your base case, simplest possible case, or divide and decrease your problem until it becomes the base case.
+
+def sum(arr):
+
+    total = 0
+
+    for x in arr:
+        total += x
+    return total
+
+print(sum([1, 2, 3, 4, 5]))
+
