a7079b427ca4b2af3a2024d70cc25e8063cc2a76
Alexander Commodore <CommodoreAlex@users.noreply.github.com>
2025-06-07 13:49:44 -0700

    Create keyValue.py

---
+++ HashMap/keyValue.py
@@ -0,0 +1,9 @@
+# Initialize the dictionary
+phonebook = {}
+
+# Fill up the dictionary with key-value pairs
+phonebook["jenny"] = "8675309"
+phonebook["emergency"] = "911"
+
+# Retrieve the value using the key
+print(phonebook["jenny"])
